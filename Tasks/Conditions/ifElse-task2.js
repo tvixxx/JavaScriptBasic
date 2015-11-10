2e08540ca98db0a56e8e3e20f8b5902ffa11cc0a
Kirill Shilov <tvix2000@yandex.ru>
2015-11-10 11:59:44 +0400

    Create ifElse-task2.js
    Get the nymber symbol - task2

---
+++ Tasks/Conditions/ifElse-task2.js
@@ -0,0 +1,10 @@
+/* Get the number symbol */
+
+var value = prompt("Please,enter the number...","");
+if ( value > 0 ) {
+   alert ( 1 );
+} else if ( value < 0 ) {
+   alert( -1 );
+} else {
+   alert ( 0 );
+}
