b56ae7b0f7a4ca6a832a5b28b7d0ce971ca08483
Kirill Shilov <tvix2000@yandex.ru>
2015-11-10 12:06:08 +0400

    Create ifElse-task4.js
     'if' in '?' - task 4

---
+++ Tasks/Conditions/ifElse-task4.js
@@ -0,0 +1,12 @@
+/* 'if' in '?' */
+
+/* Base code  */
+if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
+}
+/* End base code */
+
+/* Converted code */
+var result = (a + b < 4) ? "less" : "More";
