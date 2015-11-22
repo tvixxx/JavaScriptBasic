abd5c1aead8cc3cd9f6282a1d5bc0efbdd3c908c
Kirill Shilov <tvix2000@yandex.ru>
2015-11-22 21:07:44 +0400

    Update ReverseString.js
    added comments (rus)

--- Tasks/Method-Revese-String/ReverseString.js
+++ Tasks/Method-Revese-String/ReverseString.js
@@ -2,6 +2,7 @@
 
 /* First method  */
 function reverseString(string) {
-    return string.split("").reverse().join("");
+    return string.split("").reverse().join(""); //split('') - separator не указан, возвращает исходную строку
+    // join() - glue отсутствует, строка будет соединена запятыми.
 }
 alert(reverseString("i like HTML"));
