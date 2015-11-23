3a4454acb61666a42a5f8873ff6838de646b964d
Kirill Shilov <tvix2000@yandex.ru>
2015-11-23 19:58:12 +0400

    Update if&&.js

--- Tasks/Conditions/if&&.js
+++ Tasks/Conditions/if&&.js
@@ -9,9 +9,18 @@ alert("your age from 14 to 90" + " is " + age);
 
 /* Second task */
 /* && */
+/* first method  with (!(condition))*/
 var age = 45;
 if (!(age >= 14 && age <=90)) {
 alert("Yur value is low " + age);
 } else {
 alert("your value is upper: " + age);
 }; 
+
+/* second method with || */
+var age = 13;
+if ( age < 14 || age > 90) {
+alert("Your age is below than 14 or above than 90 is:" + " " + age);
+} else {
+alert ("Your age is above than 14 or below than 90, is:" + " " + age);
+};
