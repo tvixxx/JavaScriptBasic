272d535557254320ce622bb6a8c2f82199805e83
Kirill Shilov <tvix2000@yandex.ru>
2015-12-05 10:18:40 +0400

    Update Udacity-slice-string.js
    added second method

--- Tasks/Method-Slice-String/Udacity-slice-string.js
+++ Tasks/Method-Slice-String/Udacity-slice-string.js
@@ -1,6 +1,7 @@
 /* Task 1 */
 /* Data type - from "audacity" to "Udacity" */
 
+/* first method */
 var s = "audacity";
 
 var udacityizer = function(s) {  
@@ -9,3 +10,7 @@ var udacityizer = function(s) {
 };
 
 console.log(udacityizer(s));
+
+/* Second method */
+var s = "audacity";
+s = s[1].toUpperCase() + s.slice(2);
