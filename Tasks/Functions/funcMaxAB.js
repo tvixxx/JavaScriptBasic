3fd372a53fe9b364d72c27b2a29d41ebc13cebfd
Kirill Shilov <tvix2000@yandex.ru>
2015-12-12 18:56:28 +0400

    Update and rename funcMinAB.js to funcMaxAB.js
    *maxNumb*

--- Tasks/Functions/funcMaxAB.js
+++ Tasks/Functions/funcMaxAB.js
@@ -1,10 +1,10 @@
 /* find min number from two numbers */
 
-function minNumb(a,b) {
+function maxNumb(a,b) {
 	if (a > b) {
 		return true; //  if need to see our variable "a" write - return a;
 	} else {
 		return false; //  if need to see our variable "b" write - return b;
 	};
 };
-console.log(minNumb(5,4));
+console.log(maxNumb(5,4));
