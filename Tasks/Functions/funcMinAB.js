4272dfef042ca2bae7ad9588ca09584d0a26417b
Kirill Shilov <tvix2000@yandex.ru>
2015-12-12 18:53:57 +0400

    Update funcMinAB.js
    added comments

--- Tasks/Functions/funcMinAB.js
+++ Tasks/Functions/funcMinAB.js
@@ -2,9 +2,9 @@
 
 function minNumb(a,b) {
 	if (a > b) {
-		return true;
+		return true; //  if need to see our variable "a" write - return a;
 	} else {
-		return false;
+		return false; //  if need to see our variable "b" write - return b;
 	};
 };
 console.log(minNumb(5,4));
