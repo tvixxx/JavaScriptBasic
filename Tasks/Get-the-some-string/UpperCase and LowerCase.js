8f573a2c64f5f2eba686adbc79c9b42b99549070
Kirill Shilov <tvix2000@yandex.ru>
2016-01-14 13:18:01 +0400

    Update UpperCase and LowerCase.js

--- Tasks/Get-the-some-string/UpperCase and LowerCase.js
+++ Tasks/Get-the-some-string/UpperCase and LowerCase.js
@@ -60,20 +60,14 @@ console.log(nameChanger(name));
 var name = "AlbERt EINstEiN";
 
 function nameChanger(oldName) {
-    var finalName = oldName;
-    // Your code goes here!
 
-    
-    
-     var finalName = oldName;
+    var finalName = oldName;
     var names = oldName.split(" ");
     names[1] = names[1].toUpperCase();
     names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
     finalName = names.join(" ");
     return finalName;
-    
-    // Don't delete this line!
-    return finalName;
+
 };
 
 // Did your code work? The line below will tell you!
