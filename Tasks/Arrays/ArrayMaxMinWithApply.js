894b6466a3a8359a37ed643d9c9d33aefc99d640
Kirill Shilov <tvix2000@yandex.ru>
2016-03-22 09:40:05 +0400

    Create ArrayMaxMinWithApply.js

---
+++ Tasks/Arrays/ArrayMaxMinWithApply.js
@@ -0,0 +1,7 @@
+You can use method apply for search min and max value in array:
+
+//Min value 
+Math.min.apply(null, [29,38,115,1234,76,4,23,21,19,15,16]);
+
+//Max value
+Math.max.apply(null, [34,23,78,69,92,123,25,1,2,3,143,23,11,2,6]);
