2dca1f7c1c4ce116c5274b315bb4d34ee8d7963e
Kirill Shilov <tvix2000@yandex.ru>
2016-04-03 19:53:25 +0400

    Update getDateTime.js
    Added js method

--- Tasks/DateTask/getDateTime.js
+++ Tasks/DateTask/getDateTime.js
@@ -1,4 +1,15 @@
-var currentDate = new Date();
-var datetime = '© 2000–' + currentDate.getFullYear();
-var footerCopyrightDate = $('.footer-copyright');
-footerCopyrightDate.prepend(datetime);
+/* jQery */
+$(document).ready(function() {
+  var currentDate = new Date();
+  var datetime = '© 2000–' + currentDate.getFullYear();
+  var footerCopyrightDate = $('.footer-copyright');
+  footerCopyrightDate.prepend(datetime);
+  
+});
+
+/* js */
+ window.onload = function() {
+    var currentDate = new Date();
+    var footerCopyrightDate = document.querySelector('.footer-copyright__date');
+    footerCopyrightDate.innerHTML = '© 2000–' + currentDate.getFullYear();
+};
