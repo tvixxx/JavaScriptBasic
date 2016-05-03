5ae0dcc6ec3a783071d90d7a2ef101c73fb380a7
Kirill Shilov <tvix2000@yandex.ru>
2016-05-03 12:01:11 +0400

    Update filterArray.js

--- Tasks/Arrays/filterArray.js
+++ Tasks/Arrays/filterArray.js
@@ -1,4 +1,6 @@
-var result = arr.filter(function(value){
+var array = ['hello', 'test', 'rest', 'mest', 1, 5, '700', 600, 100500, 'wow!'];
+
+var result = array.filter(function(value){
   return (typeof(value) === 'number');
 });
 
