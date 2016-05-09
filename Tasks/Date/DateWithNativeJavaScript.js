2ec98d7e01169de9f0e70454a1e37bf43be86fca
Kirill Shilov <tvix2000@yandex.ru>
2016-05-09 11:40:39 +0400

    Update DateWithNativeJavaScript.js
    added some improvements with refactoring

--- Tasks/Date/DateWithNativeJavaScript.js
+++ Tasks/Date/DateWithNativeJavaScript.js
@@ -1,15 +1,15 @@
 var dateFromStorage = new Date(2016, 3, 1);
 
 function getDaysToday(yourDate) {
-	var day = 86400000;
-  var now = new Date();
-	console.log(now);
+	var dayMillSec = 86400000;
+  var dateNow = new Date();
+	console.log(dateNow);
 
   // создать объект из текущей даты, без часов-минут-секунд
   var dateFromStorage = yourDate;
 	console.log(dateFromStorage);
 
-  var diff = (now - dateFromStorage) / day; // разница в миллисекундах
+  var diff = (dateNow - dateFromStorage) / dayMillSec; // разница в миллисекундах
   return Math.floor(diff); // перевести в секунды
 }
 
