ac8cfae958cb537ac5376bc826f53b14f1046ad7
Kirill Shilov <tvix2000@yandex.ru>
2016-05-09 11:39:30 +0400

    Update DateWithMomentJS.js

--- Tasks/Date/DateWithMomentJS.js
+++ Tasks/Date/DateWithMomentJS.js
@@ -3,12 +3,14 @@ var twoWeek = 14;
 
 var dateFromStore = moment(new Date(2016, 3, 2));
 
-function getSecondsToday(yourDate) {
-  var dateNow = moment();
+function getSecondsToday(yourDate){
 	
-  var dateFromStore = yourDate;
+	var dateNow = moment();
+	
+	var dateFromStore = yourDate;
 	var dateFromStoreDiffResult = moment(dateNow).diff(moment(dateFromStore));
 	console.log(dateFromStoreDiffResult);
+  
 	var intervalAsDays = Math.floor(moment.duration(dateFromStoreDiffResult).as('days'));
 	console.log(intervalAsDays);
 	
@@ -19,6 +21,7 @@ function getSecondsToday(yourDate) {
 	} else {
 		console.log('Прошло несколько дней');
 	}
+	
 	return Math.floor(intervalAsDays);
 }
 
