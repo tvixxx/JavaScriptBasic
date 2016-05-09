ed4a85878f6ee779f0621d8f021ed82e8f4e48c7
Kirill Shilov <tvix2000@yandex.ru>
2016-05-09 11:42:14 +0400

    Update DateWithNativeJavaScript.js
    Added new comments and refactoring code

--- Tasks/Date/DateWithNativeJavaScript.js
+++ Tasks/Date/DateWithNativeJavaScript.js
@@ -1,22 +1,22 @@
 var dateFromStorage = new Date(2016, 3, 1);
 
-function getDaysToday(yourDate) {
+function getDaysToday(yourDate){
+	
 	var dayMillSec = 86400000;
-  var dateNow = new Date();
+	var dateNow = new Date();
 	console.log(dateNow);
 
-  // создать объект из текущей даты, без часов-минут-секунд
-  var dateFromStorage = yourDate;
+	var dateFromStorage = yourDate;
 	console.log(dateFromStorage);
 
-  var diff = (dateNow - dateFromStorage) / dayMillSec; // разница в миллисекундах
-  return Math.floor(diff); // перевести в секунды
+	var diff = (dateNow - dateFromStorage) / dayMillSec; // Конечное число дней
+	return Math.floor(diff); // Округление дней
 }
 
-if (+getDaysToday(dateFromStorage) > 14) {
-	console.log('Прошло 2 недели');
-} else if (+getDaysToday(dateFromStorage) > 7) {
-	console.log('Прошла неделя');
-} else {
-	console.log('Прошло несколько дней');
-}
+	if (+getDaysToday(dateFromStorage) > 14) {
+		console.log('Прошло 2 недели');
+	} else if (+getDaysToday(dateFromStorage) > 7) {
+		console.log('Прошла неделя');
+	} else {
+		console.log('Прошло несколько дней');
+	}
