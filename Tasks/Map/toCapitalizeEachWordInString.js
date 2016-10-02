def575b593edf894342265313c85cb4b21067f47
Kirill Shilov <tvix2000@yandex.ru>
2016-10-02 12:47:49 +0400

    Update toCapitalizeEachWordInString.js

--- Tasks/Map/toCapitalizeEachWordInString.js
+++ Tasks/Map/toCapitalizeEachWordInString.js
@@ -1,5 +1,26 @@
+// Method 1
+
 String.prototype.toJadenCase = function () {
   return this.split(' ').map(function(word){
     return word.charAt(0).toUpperCase() + word.slice(1);
   }).join(' ');
 };
+
+//Method 2
+
+String.prototype.toJadenCase = function () {
+  
+  var str = this.split(' ');
+  var strLength = str.length;
+  
+  for (var i = 0, max = strLength; i < max; i++){
+    str[i] = str[i][0].toUpperCase() + str[i].slice(1);
+  }
+  
+  return str.join(' ');
+  
+};
+
+var str = 'Hello my little friend';
+
+console.log(str.toJadenCase());
