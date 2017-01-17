d40663561ab9d343ac1e7b92fff16c3b337c1342
Kirill Shilov <tvix2000@yandex.ru>
2017-01-18 01:00:53 +0400

    Create hanaTower.js
    Добавлено 2 решения задачи по Ханойской башне.

---
+++ education/mathematic-js/hanaTower.js
@@ -0,0 +1,28 @@
+
+// Решение через цикл for
+(function() {
+  var height = 6;
+  var goal = 0;
+
+  for (var i = 0; i < height; i++) {
+     goal = 2 * goal + 1;
+  }
+  
+  console.log(`Задача имеет ${goal} решений`);
+}
+)();
+
+
+
+// Решение через рекурсию
+function recursion(height) {
+  
+  if (height == 0) {
+			return 0;  	
+  } else {
+  	return 2 * recursion(height - 1) + 1;
+  }
+  
+}
+
+console.log(recursion(3));
