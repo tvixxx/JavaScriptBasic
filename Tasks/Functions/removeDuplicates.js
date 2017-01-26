56cc8c157c49fb82383a39ff32a03bc031ffa658
Kirill Shilov <tvix2000@yandex.ru>
2017-01-26 08:39:07 +0400

    Create removeDuplicates.js

---
+++ Tasks/Functions/removeDuplicates.js
@@ -0,0 +1,36 @@
+**Условие:**
+Напишите функцию removeDuplicates, которая принимает строку и удаляет все одинаковые символы, в том случае, если они идут друг за другом.
+
+**Решение:**
+```js
+function removeDublicate(str) {
+  var newStr = '';
+  var tempStr = '';
+  
+  for (var i = 0; i < str.length; i++) {
+    if ( str[i] === tempStr) {
+      continue;
+    }
+    
+    tempStr = str[i];
+    newStr += tempStr;
+  }
+  
+  return newStr;
+}
+
+let test1 = 'Hellllllllooooo';
+let test2= 'jsssssss';
+let test3= 'tessstttttt';
+        
+console.log(removeDublicate(test1));
+console.log(removeDublicate(test2));
+console.log(removeDublicate(test3));
+```
+
+**Вывод:**
+```js
+"Helo"
+"js"
+"test"
+```
