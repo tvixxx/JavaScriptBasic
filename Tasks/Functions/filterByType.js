42598e89f27d123e60009602c0f3b9d2355ebb00
Kirill Shilov <tvix2000@yandex.ru>
2017-09-02 12:29:46 +0400

    Update filterByType.js

--- Tasks/Functions/filterByType.js
+++ Tasks/Functions/filterByType.js
@@ -1,39 +1,39 @@
 const objTypes = {
-  	number: 'number',
+    number: 'number',
     string: 'string',
     boolean: 'boolean',
-    'undefined': 'undefined',
-    'NaN': 'NaN',
-    'null': 'null'
-  }
+    undefined: 'undefined',
+    NaN: 'NaN',
+    null: 'null'
+};
 
 function filterByType() {
 
-//let args = Array.from(arguments);
-  let result = [];
-  let args = Array.prototype.slice.apply(arguments);
-  let typeForFilter = args[0];
-  let isCorrectType = checkCorrectType(typeForFilter);
-  
-  if (!isCorrectType) {
-  	 throw new Error('Введен неверный тип данных!');
-  }
-  
-  for (let i = 1; i < args.length; i++) {
-  	if (typeof args[i] === typeForFilter) {
-    	result.push(args[i]);
+    //let args = Array.from(arguments);
+    let result = [];
+    let args = Array.prototype.slice.apply(arguments);
+    let typeForFilter = args[0];
+    let isCorrectType = checkCorrectType(typeForFilter);
+
+    if (!isCorrectType) {
+        throw new Error('Введен неверный тип данных!');
     }
-  }
-  
-  return result;
+
+    for (let i = 1; i < args.length; i++) {
+        if (typeof args[i] === typeForFilter) {
+            result.push(args[i]);
+        }
+    }
+
+    return result;
 }
 
 function checkCorrectType(value) {
-	let typeOfValue = value;
-  
-	return (typeOfValue === objTypes.number ||
-  				typeOfValue === objTypes.string ||
-        	typeOfValue === objTypes.boolean);
+    let typeOfValue = value;
+
+    return (typeOfValue === objTypes.number ||
+    typeOfValue === objTypes.string ||
+    typeOfValue === objTypes.boolean);
 }
 
 filterByType('NaN', 10, 20, 'a', 'b', true, false, undefined, null, NaN);
