232d5c7fa7fc9cb88c73ba6425361c418fb31741
Kirill Shilov <tvix2000@yandex.ru>
2017-09-02 11:05:55 +0400

    Create callCodeFromUser.js

---
+++ Tasks/Functions/callCodeFromUser.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let javascriptCodeFromUser = prompt('Пожалуйста, введите JavaScript код:');
+
+function executeCode(code) {
+	'use strict';
+
+  try {
+  	eval(code);
+  } catch(e) {
+  	console.log(e, 'Возникла ошибка, не правильный код!');
+  }
+}
+
+executeCode(javascriptCodeFromUser);
