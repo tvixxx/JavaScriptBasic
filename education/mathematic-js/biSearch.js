87cab1efbc3c9de2b2296dfe2763d5f3a38ea8d6
Kirill Shilov <tvix2000@yandex.ru>
2017-11-03 12:36:07 +0400

    Updated binary search
    Added second method for binary search

--- education/mathematic-js/biSearch.js
+++ education/mathematic-js/biSearch.js
@@ -53,3 +53,44 @@ function binary_search(phonebook, lookup_person) {
 var result_binary_search = binary_search(phonebook, lookup_person);
 console.log(result_binary_search);
 console.log(pair_index);
+
+
+/* The second method, without recursion */
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+// const array = [1, 3, 5, 7, 9, 11, 13, 15];
+const noArgsMessage = 'please pass all the arguments';
+let step = 0;
+
+function binarySearch(list, target) {
+  if (!list && !target) return noArgsMessage;
+  
+  let begin = 0;
+  let end = list.length - 1;
+  let middle = 0;
+  let guess = null;
+  
+  while (begin <= end) {
+    middle = Math.floor((begin + end) / 2);
+    guess = list[middle];
+    
+    step+= 1;
+    
+    if (guess === target) {
+      return middle;
+    }
+    
+    if (guess > target) {
+      end = middle - 1;
+    }
+    
+    if (guess < target) {
+      begin = middle + 1;
+    }
+  }
+}
+
+console.log(
+  'the index of element is: ',
+  binarySearch(array, 8)
+);
+console.log('O(' + step + ')');
