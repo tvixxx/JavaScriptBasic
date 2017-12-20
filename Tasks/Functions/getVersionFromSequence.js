67d83d7d2e341bf5cafb8c85cf58842166c1f81a
Kirill Shilov <tvix2000@yandex.ru>
2017-12-20 19:30:48 +0400

    Create getVersionFromSequence.js

---
+++ Tasks/Functions/getVersionFromSequence.js
@@ -0,0 +1,21 @@
+// Write a function which can take from versions sequence.
+// function can take a string parameter.
+
+const versions = [
+  '1.0', 
+  '1.1.5',
+  '1.1.7',
+  '1.1.90',
+  '1.2',
+  '1.2.2',
+  '1.2.5',
+  '1.3.0',
+  '1.3.7'
+];
+
+function getVersion(version) {
+  const length = version.length;
+  return versions.filter(ver => ver.slice(0, length) === version).slice(-1);
+}
+
+console.log(getVersion('1.2'));
