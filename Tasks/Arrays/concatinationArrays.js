2c9fd4d9937d5ff6b4673cd8eb2369bb3940d391
Kirill Shilov <tvix2000@yandex.ru>
2018-05-01 12:05:14 +0400

    Update concatinationArrays.js

--- Tasks/Arrays/concatinationArrays.js
+++ Tasks/Arrays/concatinationArrays.js
@@ -2,25 +2,23 @@
 // inArr - main arr, which concatination with another arr
 
 //First decision
-var arr1 = [1,2,3];
-var arr2 = [4,5,6];
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
 
-function concatenationTwoArrays(inArr, sourceArr) {
-  var i = 0;
-  var arrLength = sourceArr.length;
+function concatenationTwoArrays(destinationArr, sourceArr) {
+  const arrayLength = sourceArr.length;
   
-  for (i; i < arrLength; i++) {
-    inArr.push(sourceArr[i]);
+  for (let i = 0; i < arrayLength; i++) {
+    destinationArr.push(sourceArr[i]);
   }
-  
-  return inArr;
+  return destinationArr;
 }
 
 console.log(concatenationTwoArrays(arr1, arr2)); // [1, 2, 3, 4, 5, 6];
 
 //Second decision
-var arr1 = [1,2,3];
-var arr2 = [4,5,6];
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
 
 Array.prototype.push.apply(arr1, arr2);
 console.log(arr1); // [1, 2, 3, 4, 5, 6];
