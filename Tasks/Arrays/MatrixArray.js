fedf0137fff23171fd9ca920156b4d74066b805d
Kirill Shilov <tvix2000@yandex.ru>
2018-05-01 11:57:58 +0400

    Update and rename MatrixArray to MatrixArray.js

--- Tasks/Arrays/MatrixArray.js
+++ Tasks/Arrays/MatrixArray.js
@@ -2,11 +2,11 @@
 
 var table = $('table'),
 		matrix = [
-		[3, 3, 3], 
-		[1, 5, 1], 
-		[5, 5, 5],
-		[10, 3, 8],
-		[2, 2, 2] 
+			[3, 3, 3], 
+			[1, 5, 1], 
+			[5, 5, 5],
+			[10, 3, 8],
+			[2, 2, 2] 
 		];
 
 function readAndPrintArray(array){
@@ -15,9 +15,9 @@ function readAndPrintArray(array){
 	for(var i = 0; i < arr.length; i++) {
 		
 		var tr = $('<tr>');
-		
+
 		for(var j = 0; j < 3; j++) {
-			
+
 			var td = $('<td>').text(arr[i][j]);
 			tr.append(td);
 		}
