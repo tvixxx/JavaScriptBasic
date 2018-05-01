db7ec253aa541221a0ff7f2ea69b837028b4a880
Kirill Shilov <tvix2000@yandex.ru>
2018-05-01 12:02:26 +0400

    refactoring
    a little bit refactoring of code

--- Tasks/Arrays/arrayMaxMinFor.js
+++ Tasks/Arrays/arrayMaxMinFor.js
@@ -1,10 +1,16 @@
 /* max and min with for */
 
-array = [1,2,-5,6,12,15,128,-562,31,32132314,562342341,62341234,6211,342,-3213123,-541];
+const array = [1, 2, -5, 6, 12, 15, 128, -562, 31, 32132314, 562342341, 62341234, 6211, 342, -3213123, -541];
 
 function maxSearchInArray (_array) {
-  var max = 0;
-  for (i = 0; i < _array.length; i++ ) {
+  if (!_array) {
+    return 0;
+  }
+  
+  let max = 0;
+  let arrayLength = _array.length;
+  
+  for (let i = 0; i < arrayLength; i++ ) {
    if (max < _array[i]) {
      max = array[i]; 
    }
@@ -13,17 +19,14 @@ function maxSearchInArray (_array) {
 }
 
 function minSearchInArray (_array) {
-  var min  = 0;
-  for (i = 0; i < _array.length; i++ ) {
-   if (min > _array[i]) {
-     min = array[i];
-   }
+   if (!_array) {
+    return 0;
   }
-  return min;
-}
-minSearchInArray(_array) {
-  var min  = 0;
-  for (i = 0; i < _array.length; i++ ) {
+  
+  let min  = 0;
+  let arrayLength = _array.length;
+  
+  for (let i = 0; i < arrayLength; i++ ) {
    if (min > _array[i]) {
      min = array[i];
    }
@@ -31,5 +34,8 @@ minSearchInArray(_array) {
   return min;
 }
 
-maxSearchInArray(array);// 562342341
-minSearchInArray(array); // -3213123
+const maxValueInArray = maxSearchInArray(array);// 562342341
+const minValueInArray = minSearchInArray(array); // -3213123
+
+console.log('The max value in array is: ', maxValueInArray);
+console.log('The min value in array is: ', minValueInArray);
