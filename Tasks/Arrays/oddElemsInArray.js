0752e86b73e240b743f3e72770be03732e6c6b70
Kirill Shilov <tvix2000@yandex.ru>
2018-05-01 13:12:28 +0400

    Update oddElemsInArray.js

--- Tasks/Arrays/oddElemsInArray.js
+++ Tasks/Arrays/oddElemsInArray.js
@@ -1,24 +1,21 @@
 // First method
-var q = 0; // elems
-var array = [1, 9, 3, 7, 5, 0, 2]; // our array
-for (var i = 0; i < array.length; i++) {
-    if (i % 2) { // if position is odd (1, 3, 5...)
-        if (array[i] % 2) { // if value is odd
-            q++; // increment q
-        }
+let q = 0; // elems
+const array = [1, 9, 3, 7, 5, 0, 2]; // our array
+const arrayLength = array.length;
+for (let i = 0; i < arrayLength; i++) {
+    if ((i % 2) && (array[i] % 2)) { // if position is odd (1, 3, 5...)
+       q++; // increment q
     }
 }
-alert(q);
+console.log(q);
 
 // Second method
-var q = 0;
-var array = [1,9,3,7,5,0,2];
+let q = 0;
+const array = [1,9,3,7,5,0,2];
 
-array.filter(function(valueElem, posElemInArray){
-  if (posElemInArray % 2){
-    if (valueElem % 2){
-      q++;
-    }
+array.forEach((valueElem, posElemInArray){
+  if (posElemInArray % 2 && valueElem % 2){
+    q++;
   }
 });
-alert(q);
+console.log(q);
