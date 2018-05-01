e30da42f56260ec8dbbc5792447d25928b5af790
Kirill Shilov <tvix2000@yandex.ru>
2018-05-01 12:19:26 +0400

    Update filterArray.js

--- Tasks/Arrays/filterArray.js
+++ Tasks/Arrays/filterArray.js
@@ -1,7 +1,5 @@
-var array = ['hello', 'test', 'rest', 'mest', 1, 5, '700', 600, 100500, 'wow!'];
+const array = ['hello', 'test', 'rest', 'mest', 1, 5, '700', 600, 100500, 'wow!'];
 
-var result = array.filter(function(value){
-  return (typeof(value) === 'number');
-});
+const result = array.filter(value => typeof(value) === 'number');
 
 console.log(result);
