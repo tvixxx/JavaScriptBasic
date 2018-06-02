dcdd880deb97f79d2804e91874987b6183fb7025
Kirill Shilov <tvix2000@yandex.ru>
2018-06-02 10:53:42 +0400

    Update hanoiTower.js

--- education/mathematic-js/hanoiTower.js
+++ education/mathematic-js/hanoiTower.js
@@ -1,23 +1,22 @@
 
-// Решение через цикл for
+// using for cycle
 (function() {
-  var height = 6; // Высота башни (захардкоженное число)
-  var goal = 0; // изначальное кол-во решений
+  var height = 6; // Height of tower (hardcoded number)
+  var goal = 0; // default count of decisions
 
   for (var i = 0; i < height; i++) {
-     goal = 2 * goal + 1; // формула из учебника *конкретная математика*
+     goal = 2 * goal + 1; // formula from a book *конкретная математика*
   }
   
-  console.log(`Задача имеет ${goal} решений`);
+  console.log(`The goal has ${goal} decision(s)`);
 }
 )();
 
 
 
-// Решение через рекурсию
+// Using recursion
 function recursion(height) {
-  
-  if (height == 0) {
+  if (height === 0) {
 	return 0;  	
   } else {
   	return 2 * recursion(height - 1) + 1;
