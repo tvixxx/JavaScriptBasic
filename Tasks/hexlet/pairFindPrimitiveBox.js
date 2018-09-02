b030b31b6962815a2acb8947cc44fac5b7b0e453
Kirill Shilov <tvix2000@yandex.ru>
2018-09-02 10:18:42 +0400

    Create pairFindPrimitiveBox.js

---
+++ Tasks/hexlet/pairFindPrimitiveBox.js
@@ -0,0 +1,13 @@
+Реализуйте рекурсивную функцию findPrimitiveBox, которая принимает на вход пару,
+находит внутри нее пару без вложенных пар (как описано выше) и возвращает наружу.
+
+const findPrimitiveBox = (pair) => {
+  
+  toString(pair);
+  if (!isPair(car(pair)) && !isPair(cdr(pair))) {
+      return pair;
+  }
+
+  const newPair = isPair(car(pair)) ? car(pair) : cdr(pair); 
+  return findPrimitiveBox(newPair);
+}
