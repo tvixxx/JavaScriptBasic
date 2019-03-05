967b82f5ab5ca7355a366861505afafb1d7f49a3
Kirill Shilov <tvix2000@yandex.ru>
2019-03-05 19:33:58 +0400

    Update bind.js

--- Tasks/Functions/bind.js
+++ Tasks/Functions/bind.js
@@ -1,5 +1,17 @@
+// es6
 const bind = (fn, context, ...boundArgs) => {
   return (...args) => {
     return fn.apply(context, [...boundArgs, ...args]);
   }
 }
+
+//es5
+var bind = function(fn, context) {
+  var bindArgs = [].slice.call(arguments, 2);
+
+  return function() {
+    var fnArgs = [].slice.call(arguments);
+
+    return fn.apply(context, bindArgs.concat(fnArgs));
+  };
+};
