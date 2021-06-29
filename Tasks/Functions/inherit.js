00cf4ba72678a454712335c4529d025f188ffbff
Kirill Shilov <tvix2000@yandex.ru>
2021-06-29 11:51:04 +0400

    Update inherit.js

--- Tasks/Functions/inherit.js
+++ Tasks/Functions/inherit.js
@@ -2,11 +2,14 @@ function inherit(child, parent) {
   let Func = function() {};
   Func.prototype = parent.prototype;
   child.prototype = new Func();
+  Func.prototype = null;
 }
 
 function inherit(proto) {
   let func = function Fnc() {}
   func.prototype = proto
   let object = new F;
+  func.prototype = null;
+  
   return object;
 }
